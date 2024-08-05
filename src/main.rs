071b69b2b680b9778fbdfff30e8a439539ea8f45
JuanFran Adame <juanfran.adame@gmail.com>
2024-08-05 19:07:30 +0200

    Clap arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+/// Monitoring tool for devices connected to your Ethernet network
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about = "Monitoring tool for devices connected to your Ethernet network",
+    long_about = "Argos is a monitoring tool for devices connected to your Ethernet network. It uses the ARP protocol to list devices currently connected to the network by their MAC addresses."
+)]
+struct Cli {
+    /// Minutes interval the loop is called periodically
+    #[arg(short, long, default_value_t = 1)]
+    loop_minutes: u8,
+}
+
 fn main() {
+    let args = Cli::parse();
+
+    println!("loop_minutes: {:?}", args.loop_minutes);
     println!("Hello, world!");
 }
